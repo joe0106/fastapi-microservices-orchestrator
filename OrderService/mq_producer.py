fb585de8639a22d0461613b68d9f2349586ed11e
joe0106 <beebub0106@gmail.com>
2026-05-12 16:39:57 +0800

    修改RabbitMQ使用exchange應付message fanout到兩個queue中

--- OrderService/mq_producer.py
+++ OrderService/mq_producer.py
@@ -11,11 +11,15 @@ parameters = pika.ConnectionParameters(
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
-channel.queue_declare(queue='new-order', durable=True)
+queue_names = ['new-order', 'init-ship']
+
+for queue in queue_names:
+    channel.exchange_declare(exchange="orders", exchange_type="fanout")
+    channel.queue_declare(queue=queue, durable=True)
 
 def new_order_queue_publish(obj: OrderCreateResponse):
     channel.basic_publish(
-        exchange='',
-        routing_key='new-order',
+        exchange='orders',
+        routing_key='',
         body=json.dumps(obj)
     )
