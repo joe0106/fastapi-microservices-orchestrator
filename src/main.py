555cf6c7366fd5f848d3d88a236753764726b662
joe0106 <beebub0106@gmail.com>
2026-05-28 08:21:09 +0800

    app.api_route拆開為GET POST兩個handler避免送出時混淆、proxy_request()負責送出request

--- src/main.py
+++ src/main.py
@@ -13,19 +13,40 @@ app = FastAPI()
 def sysinfo():
     return JSONResponse(content={"app_mode": settings.app_mode})
 
-@app.api_route("/{service}/{path:path}", methods=[HTTPMethod.GET, HTTPMethod.POST])
-async def generic_handler(request: Request, service: str, path: str):
-    if service == "service-a":
-        url = f"{settings.order_service_url}/order/{path}"
-        try:
-            body = await request.body()
-            response = await client.request(
+async def proxy_request(request: Request, url: str, body: any = None):
+    exclude_headers = ["host", "content-length"]
+    headers = {k: v for k, v in request.headers.items() if k.lower() not in exclude_headers}
+    
+    try:
+        response = await client.request(
                 method=request.method,
                 url=url,
-                headers=dict(request.headers),
-                content=body
+                headers=headers,
+                content= await request.body() if not body else None,
+                json=body if isinstance(body, dict) else None,
+                timeout=10
             )
-            return JSONResponse(content=response.json(), status_code=response.status_code)
+        return JSONResponse(content=response.json(), status_code=response.status_code, headers=response.headers)
+    except Exception as e:
+        return JSONResponse(content={"exception": str(e)}, status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+
+@app.get("/{service}/{path:path}")
+async def generic_handler(request: Request, service: str, path: str):
+    if service in settings.service_map().keys():
+        url = settings.build_url(service, path)
+        try:
+            return await proxy_request(request, url)
+        except Exception as e:
+            return JSONResponse(content={"exception": str(e)}, status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
+    else:
+        return JSONResponse(content={"unknown service": f"{service}"}, status_code=HTTPStatus.BAD_REQUEST)
+    
+@app.post("/{service}/{path:path}")
+async def generic_handler(request: Request, service: str, path: str, body: dict = Body(None)):
+    if service in settings.service_map().keys():
+        url = settings.build_url(service, path)
+        try:
+            return await proxy_request(request, url, body)
         except Exception as e:
             return JSONResponse(content={"exception": str(e)}, status_code=HTTPStatus.INTERNAL_SERVER_ERROR)
     else:
